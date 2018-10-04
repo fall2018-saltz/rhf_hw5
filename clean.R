8a04f96ca6fc11bd79aa43d7176bc8bda2e2c8bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:33:41 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,3 +2,23 @@
 #read in census dataset
 #dfStates <- read.csv(pop_data, stringsAsFactors = FALSE)
 print(pop_data)
+
+#Remove unnecesary rows
+dfStates <- dfStates[-c(1, 53),]
+#Remove unnecesary columns
+dfStates <- dfStates[,-c(1:4)]
+
+#utilize colnames to change the names of columns in dfStates
+cnames <- colnames(dfStates)
+cnames[1] <- "stateName"
+cnames[2] <- "population"
+cnames[3] <- "popOver18"
+cnames[4] <- "percentOver18"
+
+colnames(dfStates) <- cnames
+colnames(dfStates)
+
+#copy USArrests dataset to local variable
+arrests <- USArrests
+#convert row names of states to new column for merging
+arrests <- arrests %>% rownames_to_column("stateName")
